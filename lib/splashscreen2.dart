0dbe98c8774cc0a0c9759df9992a0f31e2255c68
whoaryu <105057945+whoaryu@users.noreply.github.com>
2024-04-09 19:03:04 +0530

    Update splashscreen2.dart

--- lib/splashscreen2.dart
+++ lib/splashscreen2.dart
@@ -1,75 +1,99 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'dart:ui';
 
-// class StackScreen extends StatelessWidget {
-//   const StackScreen({super.key});
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Stack(
-//         children: [
-//           _buildStaggeredGridView(), // Background
-//           Column(
-//             mainAxisAlignment:
-//                 MainAxisAlignment.end, // Position elements towards bottom
-//             children: [
-//               _buildImageLogos(), // Logos in the center
-//               Padding(
-//                 padding: const EdgeInsets.all(32.0),
-//                 child: Center(
-//                   child: Text(
-//                     'Ask Chitra to create any photo',
-//                     style: TextStyle(
-//                       fontSize: 24, // Adjust font size as needed
-//                       fontWeight: FontWeight.bold,
-//                       color: Colors.white,
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//               SizedBox(height: 32), // Space below text
-//               FloatingActionButton(
-//                 onPressed: () {}, // Add your skip action here
-//                 child: Text('Skip'),
-//                 backgroundColor: Colors.white,
-//               ),
-//               SizedBox(height: 32), // Bottom padding
-//             ],
-//           ),
-//         ],
-//       ),
-//     );
-//   }
+class StackScreen extends StatelessWidget {
+  const StackScreen({super.key});
 
-//   Widget _buildStaggeredGridView() {
-//     return StaggeredGridView.countBuilder(
-//       crossAxisCount: 2,
-//       itemCount: 16,
-//       itemBuilder: (context, index) =>
-//           _getImageFromAssets('pic${index + 1}'), // Dynamically fetch images
-//       staggeredTileBuilder: (index) => StaggeredTile.fit(1),
-//       mainAxisSpacing: 0.0,
-//       crossAxisSpacing: 0.0,
-//       scrollDirection: Axis.vertical,
-//       physics: AlwaysScrollableScrollPhysics(),
-//     );
-//   }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 24.0,
+            left: 24.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/welcome_to_login.svg',
+                  height: 50,
+                ),
+                const SizedBox(height: 8.0),
+                Image.asset(
+                  'assets/brand_name.png',
+                  height: 30,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 150.0,
+            left: 0.0,
+            right: 0.0,
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Text(
+                'Ask Chitra to\nCreate Any Photo',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 90.0,
+            left: 24.0,
+            child: FloatingActionButton(
+              onPressed: () {},
+              child: Icon(
+                Icons.navigate_next,
+                color: Colors.black,
+              ),
+              backgroundColor: Colors.purple,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-//   Widget _buildImageLogos() {
-//     return Center(
-//       child: Column(
-//         mainAxisSize: MainAxisSize.min,
-//         children: [
-//           Image.asset('assets/welcome_to_login.svg'),
-//           Image.asset('assets/brand_name.png'),
-//           // Add more image logo widgets as needed
-//         ],
-//       ),
-//     );
-//   }
-
-//   Widget _getImageFromAssets(String imageName) {
-//     return Image.asset('assets/$imageName.png'); // Assuming .png files
-//   }
+// Widget _buildStaggeredGridView() {
+//   return StaggeredGridView.countBuilder(
+//     crossAxisCount: 2,
+//     itemCount: 16,
+//     itemBuilder: (context, index) =>
+//         _getImageFromAssets('pic${index + 1}'), // Dynamically fetch images
+//     staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+//     mainAxisSpacing: 0.0,
+//     crossAxisSpacing: 0.0,
+//     scrollDirection: Axis.vertical,
+//     physics: AlwaysScrollableScrollPhysics(),
+//   );
 // }
+
+Widget _buildImageLogos() {
+  return Center(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset('assets/welcome_to_login.svg', height: 30),
+        Image.asset(
+          'assets/brand_name.png',
+          height: 30,
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _getImageFromAssets(String imageName) {
+  return Image.asset('assets/$imageName.png');
+}
