488d91bef02cd9881a2954468f2d7a9e49288a7d
Aryan Shah <105057945+whoaryu@users.noreply.github.com>
2024-04-10 15:03:04 +0530

    Update stilltrying.dart

--- lib/stilltrying.dart
+++ lib/stilltrying.dart
@@ -34,23 +34,94 @@ class _TrialState extends State<Trial> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: MasonryGridView.builder(
-        itemCount: 16, // Placeholder for infinite scroll implementation
-        gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-        ),
-        itemBuilder: (context, index) => Opacity(
-          opacity: 0.9,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: Image.asset(
-                'assets/pic${(_currentIndex + index) % 16 + 1}.png',
+      body: Stack(
+        children: [
+          MasonryGridView.builder(
+            itemCount: 16, // Placeholder for infinite scroll implementation
+            gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            itemBuilder: (context, index) => Opacity(
+              opacity: 0.4,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(
+                    'assets/pic${(_currentIndex + index) % 16 + 1}.png',
+                  ),
+                ),
               ),
             ),
           ),
-        ),
+          Positioned(
+            top: 20.0,
+            left: 20.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Image.asset(
+                //   'assets/welcome_to_login.svg',
+                //   height: 50,
+                // ),
+                Text(
+                  'Welcome To',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontFamily: 'Poppins-Regular',
+                  ),
+                ),
+
+                Container(
+                  width: 600,
+                  alignment: Alignment.centerLeft,
+                  child: Image.asset(
+                    'assets/brand_name.png',
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 150.0,
+            left: 0.0,
+            right: 0.0,
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Text(
+                'Ask Chitra to\nCreate Any Photo',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 90.0,
+            left: 24.0,
+            child: Row(
+              children: [
+                FloatingActionButton(
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.navigate_next,
+                    color: Colors.black,
+                  ),
+                  backgroundColor: Colors.purple,
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Text('Skip',
+                    style: TextStyle(color: Colors.white, fontSize: 20)),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
