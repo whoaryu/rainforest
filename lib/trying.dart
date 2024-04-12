0c5dd1812cfd567b1f66f9be135d8f159d5df4de
Aryan Shah <105057945+whoaryu@users.noreply.github.com>
2024-04-12 13:45:00 +0530

    Update trying.dart

--- lib/trying.dart
+++ lib/trying.dart
@@ -1,30 +1,71 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
-class trial extends StatelessWidget {
-  const trial({super.key});
+class haha extends StatefulWidget {
+  const haha({super.key});
+
+  @override
+  State<haha> createState() => _hahaState();
+}
+
+class _hahaState extends State<haha> {
+/////////////////////////////////////
+//@CodeWithFlexz on Instagram
+//
+//AmirBayat0 on Github
+//Programming with Flexz on Youtube
+/////////////////////////////////////
+  ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 1), () {
+      scrollController.animateTo(scrollController.position.maxScrollExtent,
+          duration: Duration(seconds: 16 * 10), curve: Curves.linear);
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: MasonryGridView.builder(
-        itemCount: 16,
-        gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2),
-        itemBuilder: (context, index) => Opacity(
-          opacity: 0.9,
-          child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: Image.asset(
-                  'assets/pic${index + 1}.png',
-                ),
-              )),
+      body: SizedBox(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: [
+            MasonryGridView.builder(
+              itemCount: 16,
+              gridDelegate:
+                  const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+              itemBuilder: (context, index) => Opacity(
+                opacity: 0.4,
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        'assets/pic${index + 1}.png',
+                      ),
+                    )),
+              ),
+              scrollDirection: Axis.vertical,
+              physics: AlwaysScrollableScrollPhysics(),
+            ),
+          ],
         ),
-        scrollDirection: Axis.vertical,
-        physics: AlwaysScrollableScrollPhysics(),
       ),
     );
   }
